dd7dd5c801f92a42463b8de34d7f1aa0b945795a
Bui Manh <buimanh17112001@gmail.com>
2021-05-14 01:39:43 +0700

    fix

--- src/api/story/getAll.js
+++ src/api/story/getAll.js
@@ -3,48 +3,52 @@ const getPage = require('../../utils/getPage')
 const PAGE_SIZE = 10
 
 const getAll = (req, res, next) => {
-  const { category, page, search, sort } = req.query
+  const { categories, page, search, sort } = req.query
   const { skip, limit } = getPage(page, PAGE_SIZE)
   const query = {}
 
-  if (category && category !== 'null') query.category = category
-  if (search && search !== 'null') query.text = { $regex: search, $options: 'gi'}
+  if (categories && categories !== 'null') {
+    let categoriesArray = categories.split(' ')
+    query["categories.category"] = {"$all": categoriesArray}
+}
 
-  StoryModel.find(query)
-    .populate('comments.author', 'image _id fullName')
-    .populate('follows.author', 'image _id fullName')
-    .populate('categories.category')
-    .populate('author', 'image _id fullName')
-    .sort(sort)
-    .skip(skip)
-    .limit(limit)
-    .then(resData => {
-      if (resData) {
-        StoryModel.countDocuments(query)
-          .then(count => {
-            if (count || count === 0) {
-              res.json({
-                status: true,
-                message: 'Lấy truyện thành công!',
-                stories: resData,
-                currentPage: parseInt(page),
-                totalPage: Math.ceil(count / PAGE_SIZE),
-                totalStories: count
-              })
-            } else {
-              req.err = 'Lỗi lấy truyện'
-              next('last')
-            }
-          })
-      } else {
-        req.err = 'Lỗi lấy truyện!'
-        next('last')
-      }
-    })
-    .catch(err => {
-      req.err = `Lỗi lấy truyện! + ${err}`
+if (search && search !== 'null') query["text"] = { $regex: search, $options: 'gi' }
+console.log(query)
+StoryModel.find(query)
+  .populate('comments.author', 'image _id fullName')
+  .populate('follows.author', 'image _id fullName')
+  .populate('categories.category')
+  .populate('author', 'image _id fullName')
+  .sort(sort)
+  .skip(skip)
+  .limit(limit)
+  .then(resData => {
+    if (resData) {
+      StoryModel.countDocuments(query)
+        .then(count => {
+          if (count || count === 0) {
+            res.json({
+              status: true,
+              message: 'Lấy truyện thành công!',
+              stories: resData,
+              currentPage: parseInt(page),
+              totalPage: Math.ceil(count / PAGE_SIZE),
+              totalStories: count
+            })
+          } else {
+            req.err = 'Lỗi lấy truyện'
+            next('last')
+          }
+        })
+    } else {
+      req.err = 'Lỗi lấy truyện!'
       next('last')
-    })
+    }
+  })
+  .catch(err => {
+    req.err = `Lỗi lấy truyện! + ${err}`
+    next('last')
+  })
 }
 
 module.exports = getAll
